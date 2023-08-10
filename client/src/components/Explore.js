61d874c816005e4bcc7accc1056d10c6c9c40344
Ralphie Celedon <ralphie94@pacbell.net>
2023-08-10 09:15:31 -0700

    fixed menu styling issue

--- client/src/components/Explore.js
+++ client/src/components/Explore.js
@@ -17,7 +17,38 @@ const Explore = () => {
         },
         {
             title:'Peruvian Style Rice',
-            desc:'Rice cooked with a hint of garlic and sea salt'
+            desc:'Rice cooked with a hint of garlic and sea salt.'
+        },
+        {
+            title:'Vegetables Medley',
+            desc:'A blend of baby carrots, cut corn and broccoli florets seasoned with our flavorful spices and margarine.'
+        },
+        {
+            title:'Fresh Fruit Ⓥ',
+            desc:'A blend of watermelon, cantaloupe and other abundant fruit, handpicked and cut to bite size pieces.'
+        },
+        {
+            title:'French Roll Bread Ⓥ',
+            desc:'Individually sliced pieces of French Roll Bread served in a decorative manner.'
+        }
+    ]
+
+    const french_menu = [
+        {
+            title:'Spring Mix Salad Ⓥ',
+            desc:'Consisting a variety of different lettuce, including spinach and tender baby lettuce.'
+        },
+        {
+            title:'Beef Marengo',
+            desc:'Tender, bite size pieces of beef in brown gravy and accompanied with mushrooms and tomatoes.'
+        },
+        {
+            title:'Chicken Tarragon',
+            desc:'Sauteed chicken breast cut to bite size pieces and cooked with a creamy tarragon Zinfandel sauce.'
+        },
+        {
+            title:'Rice Pilaf',
+            desc:'Rice seasoned with our flavorful spices and fine Mediterranean herbs.'
         },
         {
             title:'Vegetables Medley',
@@ -35,20 +66,39 @@ const Explore = () => {
 
   return (
     <div className='explore-container'>
+        <div className='menu-wrap'>
         <div className='menu-left'>
 
         </div>
-        <div className='menu-container'>
+        <section className='menu-container'>
         <h1>The International Menu</h1>
         <p>International cuisine that features exquisite authentic dishes from various regions around the world.</p>
-        <div className='menu-right'>
-                {int_menu_left.map((item, i) => (
-                    <div className='menu-container-right'>
-                        <p className='food-title'>{item.title}</p>
-                        <p className='food-desc'>{item.desc}</p>
-                    </div>
-                ))}
+            <div className='menu-right'>
+                    {int_menu_left.map((item, i) => (
+                        <div className='menu-container-right'>
+                            <p className='food-title'>{item.title}</p>
+                            <p className='food-desc'>{item.desc}</p>
+                        </div>
+                    ))}
+            </div>
+        </section>
         </div>
+        <div className='reverse-menu-wrap'>
+        <div className='menu-left'>
+
+        </div>
+        <section className='menu-container'>
+        <h1>The French Buffet</h1>
+        <p>A delightful array of the French styled cuisine.</p>
+            <div className='menu-right'>
+                    {french_menu.map((item, i) => (
+                        <div className='menu-container-right'>
+                            <p className='food-title'>{item.title}</p>
+                            <p className='food-desc'>{item.desc}</p>
+                        </div>
+                    ))}
+            </div>
+        </section>
         </div>
     </div>
   )
